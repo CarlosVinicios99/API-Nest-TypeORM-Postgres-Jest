bf929438656505988937a4f8ff937f62d61d611c
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-01 15:20:29 -0300

    feature: criação de rota para buscar um curso por ID

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+
+    @Get()
+    findAll(){
+        return `Listagem de cursos`
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return `Curso com ID: ${id}`
+    }
+
+}
