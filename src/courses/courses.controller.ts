1b8178669bb1a57f3ccaa50e53b4e96165646a44
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-01 20:47:55 -0300

    feature: criação de rota para cadastrar curso

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
 
-    @Get()
-    findAll(){
-        return `Listagem de cursos`
-    }
+  @Get()
+  findAll(@Res() response){
+    return response.status(200).json({message: `Listagem de cursos`})
+  }
 
-    @Get(':id')
+  @Get(':id')
     findOne(@Param('id') id: string){
-        return `Curso com ID: ${id}`
-    }
+      return `Curso com ID: ${id}`
+  }
+
+  @Post()
+  create(@Body() body){
+    return body
+  }
 
 }
