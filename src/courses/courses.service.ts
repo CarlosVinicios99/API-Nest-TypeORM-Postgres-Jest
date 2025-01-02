7dab0b4f41186d24f2405ee20cbb4f50cb0567b3
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-01 21:33:47 -0300

    feature: criação do serviço de cursos

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'NestJS',
+      description: 'Curso sobre fundamentos do framework NestJS',
+      tags: ['node.js', 'nestjs', 'javascript', 'typescript']
+    }
+  ]
+
+}
