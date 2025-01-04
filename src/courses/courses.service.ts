61a418d84fb43de1de3966cea59138d1a6180573
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-04 14:48:28 -0300

    feature: alteração na criação e atualização de curso para criar tags dinamicamente

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -2,15 +2,35 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Injectable()
 export class CoursesService {
 
   constructor(
     @InjectRepository(Course)
-    private readonly courseRepository: Repository<Course>
+    private readonly courseRepository: Repository<Course>,
+
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>
   ){}
 
+  private async preloadTagByName(name: string): Promise<Tag> {
+
+    const tag: Tag = await this.tagRepository.findOne({
+      where: {
+        name
+      }
+    })
+
+    if(tag){
+      return tag
+    }
+
+    return this.tagRepository.create({name})
+  }
 
   async findAll(){
     return await this.courseRepository.find()
@@ -31,16 +51,27 @@ export class CoursesService {
     return course
   }
 
-  async create(createCourseDTO: any){   
-    const course = this.courseRepository.create(createCourseDTO)
+  async create(createCourseDTO: CreateCourseDTO){   
+
+    const tags: Tag[] = await Promise.all(createCourseDTO.tags.map(name => this.preloadTagByName(name)))
+
+    const course = this.courseRepository.create({
+      ...createCourseDTO,
+      tags
+    })
     return await this.courseRepository.save(course)
   }
 
-  async update(id: number, updateCourseDTO: any){
+  async update(id: number, updateCourseDTO: UpdateCourseDTO){
+
+    const tags: Tag[] = 
+    updateCourseDTO.tags &&
+    (await Promise.all(updateCourseDTO.tags.map(name => this.preloadTagByName(name))))
 
     const course = await this.courseRepository.preload({
       ...updateCourseDTO,
-      id
+      id,
+      tags
     })
 
     if(!course){
