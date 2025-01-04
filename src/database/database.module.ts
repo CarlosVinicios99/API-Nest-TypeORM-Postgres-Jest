7a2de7dc64de7978e450bd162bae677a53b0ccfb
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-04 12:42:05 -0300

    config: configuração do typeorm

---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'courses',
+  entities: [],
+  synchronize: true
+}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          ...dataSourceOptions
+        }
+      }
+    })
+  ]
+})
+export class DatabaseModule {}
