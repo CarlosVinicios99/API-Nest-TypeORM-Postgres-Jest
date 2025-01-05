84edaa46b13a0609a55cdc92db9900688376eb1c
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-05 15:03:33 -0300

    feature: a criando migração para adicionar chave estrangeira de curso na tabela de relacionamento entre curso e tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from "./database.module";
 import { CreateCoursesTable1736095147864 } from "src/migrations/1736095147864-CreateCoursesTable";
 import { CreateTagsTable1736096942298 } from "src/migrations/1736096942298-CreateTagsTable";
 import { CreateCoursesTagsTable1736098557888 } from "src/migrations/1736098557888-CreateCoursesTagsTable";
+import { AddCoursesIdToCoursesTagsTable1736099386376 } from "src/migrations/1736099386376-AddCoursesIdToCoursesTagsTable";
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -10,6 +11,7 @@ export const dataSource = new DataSource({
   migrations: [
     CreateCoursesTable1736095147864, 
     CreateTagsTable1736096942298,
-    CreateCoursesTagsTable1736098557888
+    CreateCoursesTagsTable1736098557888,
+    AddCoursesIdToCoursesTagsTable1736099386376
   ]
 })
--- src/migrations/1736099386376-AddCoursesIdToCoursesTagsTable.ts
+++ src/migrations/1736099386376-AddCoursesIdToCoursesTagsTable.ts
@@ -1,11 +1,39 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
 
 export class AddCoursesIdToCoursesTagsTable1736099386376 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+            'courses_tags',
+            new TableColumn({
+                name: 'course_id',
+                type: 'uuid',
+                isNullable: true
+            })
+        )
+
+        await queryRunner.createForeignKey(
+            'courses_tags',
+            new TableForeignKey({
+                name: 'courses_tags_courses',
+                columnNames: ['course_id'],
+                referencedTableName: 'courses',
+                referencedColumnNames: ['id'],
+                onDelete: 'SET NULL'
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey(
+            'courses_tag',
+            'courses_tags_courses'
+        )
+
+        await queryRunner.dropColumn(
+            'courses_tags',
+            'course_id'
+        )
     }
 
 }
