ed6dfdefc6f1868a990ec61419802b49a97bbd7d
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-05 14:50:13 -0300

    feature: criação de migração para tababela que relaciona curso com tag

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module";
 import { CreateCoursesTable1736095147864 } from "src/migrations/1736095147864-CreateCoursesTable";
 import { CreateTagsTable1736096942298 } from "src/migrations/1736096942298-CreateTagsTable";
+import { CreateCoursesTagsTable1736098557888 } from "src/migrations/1736098557888-CreateCoursesTagsTable";
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1736095147864, CreateTagsTable1736096942298]
+  migrations: [
+    CreateCoursesTable1736095147864, 
+    CreateTagsTable1736096942298,
+    CreateCoursesTagsTable1736098557888
+  ]
 })
---
+++ src/migrations/1736099386376-AddCoursesIdToCoursesTagsTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddCoursesIdToCoursesTagsTable1736099386376 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
