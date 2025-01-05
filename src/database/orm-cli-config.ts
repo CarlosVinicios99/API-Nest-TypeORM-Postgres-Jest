8d7e53e3b6b17fb52bc0ca94e03e9e9143708a78
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-05 14:20:00 -0300

    feature: criação da migração da tabela de tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module";
 import { CreateCoursesTable1736095147864 } from "src/migrations/1736095147864-CreateCoursesTable";
+import { CreateTagsTable1736096942298 } from "src/migrations/1736096942298-CreateTagsTable";
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1736095147864]
+  migrations: [CreateCoursesTable1736095147864, CreateTagsTable1736096942298]
 })
