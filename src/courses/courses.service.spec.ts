7910b7116a8bbed2ec8edc7c34e3aa91af51e83d
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-07 22:19:14 -0300

    test: criação de teste para atualizar curso

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesService } from './courses.service';
 import { randomUUID } from 'node:crypto';
 import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 describe('CoursesService unit tests', () => {
   let service: CoursesService;
@@ -107,5 +108,29 @@ describe('CoursesService unit tests', () => {
     expect(mockCourseRepository.findOne).toHaveBeenCalled()
     expect(expectOutputCourses).toStrictEqual(course)
   })
+
+  it('should update a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagsRepository
+
+    const updateCourseDTO: UpdateCourseDTO = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs']
+    }
+
+    const course = await service.update(id, updateCourseDTO)
+
+    expect(mockCourseRepository.preload).toHaveBeenCalled()
+    expect(mockCourseRepository.save).toHaveBeenCalled()
+
+    expect(expectOutputCourses).toStrictEqual(course)
+  })
+
+
+
 
 });
